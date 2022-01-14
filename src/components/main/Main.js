857b59d8ab2e96bdcf4802fe0cf9c08697ac31f6
Charlene Kwok <charlenekwok499@gmail.com>
2022-01-14 17:40:51 +1100

    add pics on main page

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -1,10 +1,22 @@
 import React from "react";
-import PropTypes from "prop-types";
+import "./Main.css";
+import photo from "../../assets/illustrations/image1.jpeg";
 
 const Main = (props) => {
-  return <div></div>;
+  return (
+    <div className="main-page">
+      <div>
+        <div className="main-page__image--container">
+          <img src={photo} alt="illustration" width="100%" height="100%" />
+          {/* <div className="main-page__image"></div> */}
+        </div>
+      </div>
+      <div className="main-page__text">
+        <h1>Hi I'm Charlene</h1>
+        <p></p>
+      </div>
+    </div>
+  );
 };
-
-Main.propTypes = {};
 
 export default Main;
